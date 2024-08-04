30dd808bbffe5424e7aa700a327c88eb96ed4099
NamitSingh <38458966+namitsingh09@users.noreply.github.com>
2024-08-04 12:47:51 +0530

    added project

--- app/Projects/page.js
+++ app/Projects/page.js
@@ -223,6 +223,26 @@ export default function page() {
             </button>
             <div class="absolute inset-0 bg-gradient-to-b from-black opacity-0 group-hover:opacity-40"></div>
             </a>
+
+            <a href="https://github.com/vpnwann/AmazonClone"
+                class="group relative w-200 flex h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg md:h-80">
+                <img src="https://firebasestorage.googleapis.com/v0/b/abup-e3b67.appspot.com/o/Screenshot%202024-08-04%20124213.png?alt=media&token=9ebda645-e349-41e2-a232-6d3caf0b1933" loading="lazy" width={200}  height={200} alt="Photo by Minh Pham" class="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110" />
+
+                <div
+                    class="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-80">
+                </div>
+
+                <span class="relative ml-4 mb-3 inline-block text-sm text-white  md:ml-5 md:text-lg">AmazonClone</span>
+                <button class="absolute inset-0 flex items-center justify-center text-white opacity-0 group-hover:opacity-100" onclick="navigateToLink('/About')">
+               
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
+  <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+</svg>
+
+            </button>
+            <div class="absolute inset-0 bg-gradient-to-b from-black opacity-0 group-hover:opacity-40"></div>
+            </a>
            
           
 
@@ -230,6 +250,8 @@ export default function page() {
             
             
         </div>
+
+        
 
 
 
